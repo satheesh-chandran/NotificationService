30721e0b3a81caf8c16350250ee29c96e3a5aede
Satheesh <58027206+satheesh-chandran@users.noreply.github.com>
2021-04-14 12:21:07 +0530

    Get message and title from the notification

--- app/src/main/java/com/example/notification/notificationservice/NLService.java
+++ app/src/main/java/com/example/notification/notificationservice/NLService.java
@@ -1,13 +1,27 @@
 package com.example.notification.notificationservice;
 
+import android.app.Notification;
 import android.content.Intent;
+import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
+import android.widget.RemoteViews;
+
+import java.util.List;
 
 public class NLService extends NotificationListenerService {
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
         super.onNotificationPosted(sbn);
+        Notification notification = sbn.getNotification();
+        Bundle extras = notification.extras;
+        String title = extras.getString(Notification.EXTRA_TITLE);
+        String text = extras.getString(Notification.EXTRA_TEXT);
+
+        Log.d("Satheesh", title);
+        Log.d("Satheesh", text);
+
         Intent intent = new Intent("com.example.notification.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
         intent.putExtra("MESSAGE", "DATA passed from Service");
         sendBroadcast(intent);
